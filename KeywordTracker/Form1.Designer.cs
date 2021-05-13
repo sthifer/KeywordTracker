6707091d69c0c753edcad8793836b56cf3703cf0
Javier Martínez <sthifer21@gmail.com>
2021-05-13 21:52:09 +0200

    Agregar archivos de proyecto.

---
+++ KeywordTracker/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace KeywordTracker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buscar = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buscar
+            // 
+            this.buscar.Location = new System.Drawing.Point(341, 185);
+            this.buscar.Name = "buscar";
+            this.buscar.Size = new System.Drawing.Size(75, 23);
+            this.buscar.TabIndex = 0;
+            this.buscar.Text = "button1";
+            this.buscar.UseVisualStyleBackColor = true;
+            this.buscar.Click += new System.EventHandler(this.buscar_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(198, 250);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(376, 47);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(265, 65);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buscar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buscar;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ KeywordTracker/Form1.cs
@@ -0,0 +1,80 @@
+﻿
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+using ScrapySharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeywordTracker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buscar_Click(object sender, EventArgs e)
+        {
+
+            //System.Net.WebClient client = new WebClient();
+            //client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12");
+            //client.Headers.Add("Accept", "*/*");
+            //client.Headers.Add("Accept-Language", "en-gb,en;q=0.5");
+            //client.Headers.Add("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.7");
+
+            //string reply = client.DownloadString("https://www.google.es/search?q=navaleno+que+ver");
+
+            //var jsonresult = JsonConvert.DeserializeObject<reply>(result);
+            string url = textBox1.Text.Replace(" ","+");
+            //Console.WriteLine(reply);
+            List<string> MisTitulos = new List<string>();
+            HtmlWeb oWeb = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = oWeb.LoadFromBrowser("https://www.google.com/search?q="+ url);
+
+            //HtmlNode Body = doc.DocumentNode.CssSelect("body").First();
+
+            //string sBody = Body.InnerHtml;
+
+            foreach (var Nodo in doc.DocumentNode.CssSelect(".yuRUbf"))
+            {
+                MisTitulos.Add(Nodo.InnerText);
+                //MisTutulos.Add(doc.DocumentNode.SelectNodes("<a href=").First().InnerText);
+            }
+
+            int i = 1;
+            foreach (var listado in MisTitulos)
+            {
+                //listado.Contains("jmffdftravel.es");
+                //int compara = string.Compare(listado, "jmffdftravel.es");
+                if (listado.Contains("jmtravel.es"))
+                {
+                    richTextBox1.Text = "La Key " + textBox1.Text + " se encuentra en la posición:" + i;
+                    break;
+                }
+                i++;
+            }
+
+            //Console.WriteLine(MisTutulos);
+        }
+    }
+}
