b71b1a629ccf413741634843d175051d35542bb0
Javier Martínez <sthifer21@gmail.com>
2021-05-14 14:54:06 +0200

    Varios
    Añadir lectura excel
    Intertar en la tabla
    Recorrer la tabla
    Buscar las keys una por una

--- KeywordTracker/Form1.Designer.cs
+++ KeywordTracker/Form1.Designer.cs
@@ -32,11 +32,17 @@ namespace KeywordTracker
             this.buscar = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Keyword = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Dominio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.posihoy = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.procesar_todo = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // buscar
             // 
-            this.buscar.Location = new System.Drawing.Point(341, 185);
+            this.buscar.Location = new System.Drawing.Point(282, 34);
             this.buscar.Name = "buscar";
             this.buscar.Size = new System.Drawing.Size(75, 23);
             this.buscar.TabIndex = 0;
@@ -46,7 +52,7 @@ namespace KeywordTracker
             // 
             // richTextBox1
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(198, 250);
+            this.richTextBox1.Location = new System.Drawing.Point(412, 22);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(376, 47);
             this.richTextBox1.TabIndex = 1;
@@ -54,23 +60,66 @@ namespace KeywordTracker
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(265, 65);
+            this.textBox1.Location = new System.Drawing.Point(12, 34);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(237, 20);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Keyword,
+            this.Dominio,
+            this.posihoy});
+            this.dataGridView1.Location = new System.Drawing.Point(35, 121);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(697, 150);
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Keyword
+            // 
+            this.Keyword.HeaderText = "Keyword";
+            this.Keyword.Name = "Keyword";
+            // 
+            // Dominio
+            // 
+            this.Dominio.HeaderText = "Dominio";
+            this.Dominio.Name = "Dominio";
+            // 
+            // posihoy
+            // 
+            this.posihoy.HeaderText = "Posición Hoy";
+            this.posihoy.Name = "posihoy";
+            // 
+            // procesar_todo
+            // 
+            this.procesar_todo.Location = new System.Drawing.Point(282, 309);
+            this.procesar_todo.Name = "procesar_todo";
+            this.procesar_todo.Size = new System.Drawing.Size(75, 23);
+            this.procesar_todo.TabIndex = 4;
+            this.procesar_todo.Text = "button1";
+            this.procesar_todo.UseVisualStyleBackColor = true;
+            this.procesar_todo.Click += new System.EventHandler(this.procesar_todo_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.procesar_todo);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.buscar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -81,6 +130,11 @@ namespace KeywordTracker
         private System.Windows.Forms.Button buscar;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Keyword;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Dominio;
+        private System.Windows.Forms.DataGridViewTextBoxColumn posihoy;
+        private System.Windows.Forms.Button procesar_todo;
     }
 }
 
--- KeywordTracker/Form1.cs
+++ KeywordTracker/Form1.cs
@@ -1,4 +1,5 @@
 ﻿
+using ExcelDataReader;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
 using ScrapySharp.Extensions;
@@ -7,6 +8,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
@@ -20,7 +22,7 @@ namespace KeywordTracker
         public Form1()
         {
             InitializeComponent();
-
+            Cargar_Excel();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -32,7 +34,27 @@ namespace KeywordTracker
         {
 
         }
+        private void Cargar_Excel()
+        {
+            //Ruta del fichero Excel
+            string filePath = @"C:\Users\Javier\Desktop\Keywords.xlsx";
 
+            using (var stream = File.Open(filePath, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    do
+                    {
+                        reader.Read();
+                        while (reader.Read())
+                        {
+                            dataGridView1.Rows.Add(reader.GetString(0), reader.GetString(1), reader.GetString(2));
+                        }
+                    } while (reader.NextResult());
+
+                }
+            }
+        }
         private void buscar_Click(object sender, EventArgs e)
         {
 
@@ -75,6 +97,49 @@ namespace KeywordTracker
             }
 
             //Console.WriteLine(MisTutulos);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void procesar_todo_Click(object sender, EventArgs e)
+        {
+            int ifor = 0;
+            for (ifor = 0; ifor < dataGridView1.Rows.Count; ifor++)
+            {
+                string url = dataGridView1.Rows[ifor].Cells[0].Value.ToString().Replace(" ", "+");
+                //string url = url.Replace(" ", "+");
+                List<string> MisTitulos = new List<string>();
+                HtmlWeb oWeb = new HtmlWeb();
+                HtmlAgilityPack.HtmlDocument doc = oWeb.LoadFromBrowser("https://www.google.com/search?q=" + url);
+
+                //HtmlNode Body = doc.DocumentNode.CssSelect("body").First();
+
+                //string sBody = Body.InnerHtml;
+
+                foreach (var Nodo in doc.DocumentNode.CssSelect(".yuRUbf"))
+                {
+                    MisTitulos.Add(Nodo.InnerText);
+                    //MisTutulos.Add(doc.DocumentNode.SelectNodes("<a href=").First().InnerText);
+                }
+
+                int i = 1;
+                foreach (var listado in MisTitulos)
+                {
+                    //listado.Contains("jmffdftravel.es");
+                    //int compara = string.Compare(listado, "jmffdftravel.es");
+                    if (listado.ToLower().Contains(dataGridView1.Rows[ifor].Cells[1].Value.ToString().ToLower()))
+                    {
+                        //richTextBox1.Text += "La Key " + textBox1.Text + " se encuentra en la posición:" + i;
+                        dataGridView1.Rows[ifor].Cells[2].Value = i;
+                        break;
+                    }
+                    i++;
+                }
+            }
+
         }
     }
 }
